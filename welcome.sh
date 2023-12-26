579f7bfeb0ea22f672f51943b1bcfee9c85a2f32
Oleg Lukianov <oleg.lukianov8@gmail.com>
2023-12-26 16:34:10 +0200

    Update welcome

--- welcome.sh
+++ welcome.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-version="1.0.1";
+
+version="1.0.1 20.01.2018";
 version="1.0.2 23.01.2020";
 version="1.0.3 22.09.2020";
+# Changelog:
+# - getInterface modifyed
 version="1.0.4 26.12.2023";
+# Changelog:
+# - SWAP added
+# - Android and linux versions merged
+
+# DEBUG_MODE: 0-disable, 1-enable
+DEBUG_MODE=0
 
 function getHostname() {
     hostname=$(hostname)
@@ -10,31 +19,64 @@ function getHostname() {
 }
 
 function getInterface() {
-    inet=$(/usr/sbin/ifconfig -a | grep -oE "^[a-z0-9]+" | uniq);
+    step_inter=1;
+    step_ip=0;
     mass=();
+    mass_inter=();
+    mass_ip=();
 
-    for interface in $inet; do
-        ip=$(/usr/sbin/ifconfig "$interface" | grep inet | grep -i mask | sed 's/[a-zA-Z:]//g' | awk '{print $1}' | grep -v 127.0.0.1);
-        if [ "$ip" ]; then
-            mass+=("$interface ($ip)");
-        fi
+    IFS=$'\n';
+    for x in $(ifconfig -a 2>/dev/null); do
+        interface=$(echo "$x" | grep -oE "^[a-z0-9_]+");
+        ip_addrr=$(echo "$x" | grep inet | awk '{print $2}');
+        mass_inter[$step_inter]=$interface;
+        mass_ip[$step_ip]="$ip_addrr";
+        (( step_inter+=1 ))
+        (( step_ip+=1 ))
+        if [[ $DEBUG_MODE == 1 ]]; then
+            echo "$interface ($ip_addrr)";
+        fi;
+    done;
+    unset IFS
+
+    for y in $(seq 0 $step_inter); do
+        if [[ "${mass_ip[$y]}" != "127.0.0.1" ]]; then
+            if [[ ${mass_ip[$y]} =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+            if [[ $DEBUG_MODE == 1 ]]; then
+                echo "${mass_inter[$y]} ${mass_ip[$y]}"
+            fi;
+            mass+=("${mass_inter[$y]} (${mass_ip[$y]})");
+            fi;
+        fi;
     done;
 
     echo "${mass[@]}"
 }
 
 function getKernel() {
-    kernel=$(uname -a | awk '{print $15" "$3" "$5" "$14}')
-    echo "$kernel"
+    kernel_s=$(uname -s)
+    kernel_n=$(uname -n)
+    kernel_r=$(uname -r)
+    kernel_m=$(uname -m)
+    kernel_o=$(uname -o)
+    echo "$kernel_s $kernel_n $kernel_r $kernel_m $kernel_o"
 }
 
 function getOS() {
-    os=$(cat /etc/system-release)
+    if [[ -f "/etc/system-release" ]]; then
+        os=$(cat /etc/system-release)
+    else
+        os_devicename=$(getprop ro.config.devicename | getprop ro.product.model)
+        os_name=$(getprop net.bt.name)
+        os_release=$(getprop ro.build.version.release)
+        os="$os_devicename - $os_name $os_release"
+    fi;
+
     echo "$os"
 }
 
 function getUptime() {
-    uptime=$(uptime | sed 's/^\s//g')
+    uptime=$(uptime | sed 's/^\s//g' | sed 's/  / /g')
     echo "$uptime"
 }
 
@@ -44,22 +86,22 @@ function getCPUcount() {
 }
 
 function getCPUmodel() {
-    model=$(grep "model name" /proc/cpuinfo | head -1 | awk '{print $4" "$5" "$6" "$7" "$8" "$9" "$10}')
+    model=$(lscpu | grep 'Model name' | sed 's/Model name:\s//g' | uniq | xargs)
     echo "$model"
 }
 
 function getMemory() {
-    memory=$(cat /proc/meminfo | grep MemTotal | awk '{printf"%d", $2/1024}')
+    memory=$(grep MemTotal /proc/meminfo | awk '{printf"%d", $2/1024}')
     echo "$memory Mb"
 }
 
 function getSwap() {
-    swap=$(cat /proc/meminfo | grep SwapTotal | awk '{printf"%d", $2/1024}')
+    swap=$(grep SwapTotal /proc/meminfo | awk '{printf"%d", $2/1024}')
     echo "$swap Mb"
 }
 
 echo -e "\e[0;91m
-++++++++++++++++++: System Data (version $version) :++++++++++++++++++\e[0m
+++++++++++++++++++++: System Data (version $version) :++++++++++++++++++++\e[0m
 \e[0;94m    Hostname\e[0m = \e[0;93m$(getHostname) \e[0m
 \e[0;94m     Address\e[0m = \e[0;93m$(getInterface) \e[0m
 \e[0;94m      Kernel\e[0m = \e[0;93m$(getKernel) \e[0m
@@ -68,6 +110,6 @@ echo -e "\e[0;91m
 \e[0;94m         CPU\e[0m = \e[0;93m$(getCPUcount) x $(getCPUmodel) \e[0m
 \e[0;94m      Memory\e[0m = \e[0;93m$(getMemory) \e[0m
 \e[0;94m        Swap\e[0m = \e[0;93m$(getSwap) \e[0m
-\e[0;91m++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\e[0m"
+\e[0;91m++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\e[0m"
 
 exit 0;
